9f9b3d24dc7ce13f07312a1a3db578e93c45b8af
root <451080617@qq.com>
2020-12-24 16:27:17 +0800

    test

--- day2/commonjs.js
+++ day2/commonjs.js
@@ -19,4 +19,4 @@ http.createServer(function (request, response) {
     response.end(apd.apd2(request.url)); */
 }).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081/ ');
